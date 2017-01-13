1559633531cbbfe555e1d782093467421a8b43b0
Max Gallagher <maxgallagher@tests-MacBook-Pro.local>
2017-01-12 21:27:52 -0600

    Initial Commit

---
+++ HelloooWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloooWorld
+//
+//  Created by Max Gallagher on 1/12/17.
+//  Copyright © 2017 Max Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
